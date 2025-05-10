e606b5c3daf5aa39848ac7a17e84f1975746ecfb
Kauan2ek <kauantallessilvadias@gmail.com>
2025-05-10 17:33:52 -0300

    Estruturação de pastas/projeto

---
+++ assets/js/produtos.js
@@ -0,0 +1,28 @@
+// console.log("Produtos.js carregou!");
+
+// const blocoItens = document.querySelector(".bloco-itens");
+// // const teste = document.querySelector("#teste");
+
+// const item = document.createElement("div");
+// item.classList.add("item");
+
+// console.log(item);
+
+// blocoItens.append("item");
+
+// async function carregarJSON() {
+//     const response = await fetch('../assets/json/produtos.json');
+//     const data = await response.json();
+//     produtos = data.produtos;
+//     console.log(produtos[0].img);
+//     // console.log(produtos);
+//     // teste.style.backgroundImage = `url(${produtos[0].img})`;
+    
+// }
+
+// carregarJSON();
+
+
+const itens = document.querySelectorAll(".item");
+
+console.log(itens[0]);
