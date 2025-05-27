85696769f66334b7dc72eea3f06d9daef207d027
Kauan Talles <111541085+Kauan2ek@users.noreply.github.com>
2025-05-27 14:48:13 -0300

    ...

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,18 +1,20 @@
-   /**
-     * Alterna a visibilidade do menu flutuante
-     */
-    function toggleMenu() {
-      const menu = document.getElementById('menuLinks');
-      menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
-    }
+/**
+  * Alterna a visibilidade do menu flutuante
+  */
+function toggleMenu() {
+  const menu = document.getElementById('menuLinks');
+  menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+}
 
-    /**
-     * Fecha o menu se o usuário clicar fora dele
-     */
-    window.addEventListener('click', function (e) {
-      const menu = document.getElementById('menuLinks');
-      const button = document.querySelector('.botao-fixo');
-      if (!menu.contains(e.target) && !button.contains(e.target)) {
-        menu.style.display = 'none';
-      }
-    });
+/**
+ * Fecha o menu se o usuário clicar fora dele
+ */
+window.addEventListener('click', function (e) {
+  console.log("AAAA");
+  const menu = document.getElementById('menuLinks');
+  console.log(menu);
+  const button = document.querySelector('.botao-fixo');
+  if (!menu.contains(e.target) && !button.contains(e.target)) {
+    menu.style.display = 'none';
+  }
+});
