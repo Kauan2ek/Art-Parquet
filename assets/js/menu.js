c3abfff7762e0555b0d736bf1663a5239cc2a80e
Kauan Talles <111541085+Kauan2ek@users.noreply.github.com>
2025-05-27 15:03:56 -0300

    Arrumando dados do menu

---
+++ assets/js/menu.js
@@ -0,0 +1,20 @@
+/**
+  * Alterna a visibilidade do menu flutuante
+  */
+function toggleMenu() {
+  const menu = document.getElementById('menuLinks');
+  menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
+}
+
+/**
+ * Fecha o menu se o usuário clicar fora dele
+ */
+window.addEventListener('click', function (e) {
+  console.log("AAAA");
+  const menu = document.getElementById('menuLinks');
+  console.log(menu);
+  const button = document.querySelector('.botao-fixo');
+  if (!menu.contains(e.target) && !button.contains(e.target)) {
+    menu.style.display = 'none';
+  }
+});
